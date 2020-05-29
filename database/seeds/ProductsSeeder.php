25dec33e8e7dcd2f86f161b3d15602c83cc3e41f
Houssam Abd El Moniem <31729534+Houssam5@users.noreply.github.com>
2020-05-29 21:24:41 +0200

    updating seerder image

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -16,101 +16,101 @@ class ProductsSeeder extends Seeder
         DB::table('products')->insert([
             'name' => 'Classic',
             'description' => 'BeffPatty,chedderCheese,grilled Onion , cocktailSauce.',
-            'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/935-classic-burger.png',
+            'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
             'price' => 20.00
          ]);
-       
+
          DB::table('products')->insert([
              'name' => 'Classic Cheese',
              'description' => 'doubleBeef, tomato, cocktail sauce , cocktailSauce.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/941-classic-cheese.png',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 30.00
          ]);
-  
+
          DB::table('products')->insert([
              'name' => 'Classic Double',
              'description' => 'doubleBeef,chedderCheese,grilled Onion , cocktailSauce.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/947-double-classic.png',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 35.00
          ]);
-  
+
          DB::table('products')->insert([
              'name' => 'Classic Double Cheese',
              'description' => 'doubleBeef,doubleCheese,chedder Cheese, grilled onion.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/953-classic-double-cheese.png',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 40.00
          ]);
-  
+
          DB::table('products')->insert([
              'name' => 'Italian style',
              'description' => 'beefPatty,fresh mozzarella italian salami, cocktailSauce.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/959-italian-style.png',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 43.50
          ]);
          DB::table('products')->insert([
              'name' => 'Mozzarella Soft',
              'description' => 'beefpatty,freshmozzarella,tomato, cocktailSauce,onion.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/965-mozzarella-soft.png',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 40.50
          ]);
          DB::table('products')->insert([
              'name' => ' Blue cheese',
              'description' => 'beefpatty,bluecheese ,cocktail Sauce, onion,cheese.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/971-blue-cheese.png',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 45.00
          ]);
          DB::table('products')->insert([
              'name' => ' Chedder Cheese',
              'description' => 'beefpatty,bacon,cheddarCheese, cocktailSauce, cheese.',
-             'photo' => ' https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/977-bacon-and-cheddar-cheese.png',
+             'photo' => ' https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 50.00
          ]);
          DB::table('products')->insert([
              'name' => 'Swiss Cheese',
              'description' => 'beefpatty,bacon,cheddarcheese, lettuce, onions, chesse.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/983-swiss-cheese.png',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 50.00
          ]);
          DB::table('products')->insert([
              'name' => 'Grilled Chicken',
              'description' => 'grilledchicken breast,cheddar cheese ,white oregano.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/989-grilled.png',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 20.00
          ]);
          DB::table('products')->insert([
              'name' => 'Pepper Cheese',
              'description' => 'beef patty, cheddar cheese, tomato, onion, red paper.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/994-pepper----cheese.png ',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY= ',
              'price' => 40.00
          ]);
          DB::table('products')->insert([
              'name' => 'Veggier',
              'description' => 'freshgrilledvegetables,broccoli, tomato,freshcucumber.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/1000-veggie.png',
-             'price' => 30.00 
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
+             'price' => 30.00
          ]);
          DB::table('products')->insert([
              'name' => 'Mignon Steak',
              'description' => 'mignonsteak,buildyours,tomato, freshcucumber , onion.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/1005-midnon-steak.png',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 20.00
          ]);
          DB::table('products')->insert([
              'name' => 'BBQ Grilled Chicken',
              'description' => 'grilledchicken breast,rucola and barbecue sauce.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/1010-bbq-grilled-chicken.png',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 30.00
          ]);
          DB::table('products')->insert([
              'name' => 'Pepper Steak',
              'description' => 'cognac,peppersauce,grilledpepper, cheddar cheese.',
-             'photo' => 'https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/1015-pepper-steak.png',
+             'photo' => 'https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 35.00
          ]);
          DB::table('products')->insert([
              'name' => 'Tuscan Style',
              'description' => 'tomato,rucola,figs,prosciutto, toma, fresh cucumber.',
-             'photo' => ' https://static.ucraft.net/fs/userFiles/demo-burgerbar/images/1020-tuscan----style.png',
+             'photo' => ' https://media.istockphoto.com/photos/double-cheese-burger-picture-id945057664?k=6&m=945057664&s=612x612&w=0&h=RUo4h_hgeBaExePasl-2YDhEMp4iKKScKpxX_mXlJVY=',
              'price' => 40.00
          ]);
     }
