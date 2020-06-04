cdd2486df80be0d9185de16b7144f86e246ccf4c
asmaamohamed22 <asmaamohamed22299@gmail.com>
2020-06-04 20:52:58 +0200

    update action

--- resources/views/remove2.blade.php
+++ resources/views/remove2.blade.php
@@ -17,7 +17,7 @@
       <th scope="col">User Name</th>
       <th scope="col">User Password</th>
       <th scope="col">Work Shift</th>
-      <th scope="col"></th>
+      <th scope="col">Action</th>
 
     </tr>
   </thead>
